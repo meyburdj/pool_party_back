b2f1b378982032793fce4cff6a22773bf8bcefba
Christopher Maltez <cflux.maltez@live.com>
2023-03-16 22:21:49 -0700

    wip - workin on resizing uploaded img to resize to smaller and maintaining reasonable sharpness

--- api_helpers.py
+++ api_helpers.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import boto3
 from werkzeug.utils import secure_filename
 import uuid
+from PIL import Image
+
 
 load_dotenv()
 
@@ -10,9 +12,12 @@ aws_access_key_id = os.environ['aws_access_key_id']
 aws_secret_access_key = os.environ['aws_secret_access_key']
 
 s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
-BUCKET_NAME='sharebnb-gmm'
+BUCKET_NAME_LARGE_IMAGES='sharebnb-gmm'
+BUCKET_NAME_SMALL_IMAGES='sharebnb-gmm-small-images'
 
-bucket_base_url = "https://sharebnb-gmm.s3.us-west-1.amazonaws.com/"
+
+bucket_base_url_large_images = "https://sharebnb-gmm.s3.us-west-1.amazonaws.com/"
+bucket_base_url_small_images = "https://sharebnb-gmm-small-images.s3.us-west-1.amazonaws.com/"
 
 
 def upload_to_aws(file):
@@ -22,10 +27,42 @@ def upload_to_aws(file):
 
     s3.upload_fileobj(
         file,
-        Bucket = BUCKET_NAME,
+        Bucket = BUCKET_NAME_LARGE_IMAGES,
         ExtraArgs={"ContentType": "mimetype"},
         Key = filename
     )
-    url = f"{bucket_base_url}{filename}"
+    url = f"{bucket_base_url_large_images}{filename}"
+
+    small_image_file = resize_image(file)
+
+    s3.upload_fileobj(
+        small_image_file,
+        Bucket = BUCKET_NAME_SMALL_IMAGES,
+        ExtraArgs={"ContentType": "mimetype"},
+        Key = filename
+    )
 
     return url
+
+
+def resize_image(file):
+    """ Resizes image to height of 140 """
+
+    # need something like with Image.open(blah)
+    with Image.open(file) as img: # test this code
+        # img = Image.open(file)
+        orig_height = int(img.height)
+        orig_width = int(img.width)
+
+        # orig_aspect_ratio = orig_height / orig_width
+
+        orig_height_to_140_ratio = orig_height / 140
+        new_height = 140
+        new_width = int(orig_width / orig_height_to_140_ratio)
+
+        # height needs to be 140
+        resized_img = img.resize((new_width, new_height), resample=Image.BICUBIC)
+        # resized_img.show()
+
+        return resized_img
+
